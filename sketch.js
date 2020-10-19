ad1ad00b9ebacf84e839716849981ef968bfefee
nfl-r3al1ty <69349650+nfl-r3al1ty@users.noreply.github.com>
2020-10-19 17:07:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+//Create variables here
+var dog, happyDog;
+var database;
+var foodS, foodStock;
+
+function preload()
+{
+  //load images here
+  loadImage(images/dogImg.png);
+  loadImage(images/dogImg1.png);
+}
+
+function setup() {
+  createCanvas(500, 500);
+  
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
